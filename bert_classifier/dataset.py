3442908e62decfd191801d4ef1177782ca63485c
Ming-Chieh Liu <mingchiehliu@fe01>
2024-01-07 01:34:56 -0600

    custom dataset class for annotated solution ratings

---
+++ bert_classifier/dataset.py
@@ -0,0 +1,22 @@
+from torch.utils.data import Dataset
+from pathlib import Path
+import pandas as pd 
+import torch
+
+class HackathonDataset(Dataset):
+    """custom dataset class that reads in {problem, solution} pairs"""
+    def __init__(self, data_path: Path):
+        self.data = pd.read_csv(data_path, encoding="latin1")
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        problem = self.data.loc[idx, "Problem"]
+        solution = self.data.loc[idx, "Solution"]
+
+        # concatenate the scores from three criteria 
+        label = self.data.loc[idx, ["Circular Economy", "Market Potentials", "Feasibility"]]
+
+        # return: (str) {problem, solution} pair, (Tensor[float]) ordering score from 1-5 for three criteria
+        return "Problem: " + str(problem) + "\nSolution: " + str(solution), torch.tensor(label).to(torch.long)*2-2
