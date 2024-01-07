83cc6e33fecebbc0b0cc4b282419f4b6579e8457
wvjug <wv.juganda@gmail.com>
2024-01-07 14:33:06 -0600

    Fixed UI

--- chat_ui.py
+++ chat_ui.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from bert_classifier.eval import eval
+from gpt_chatbot.tinyllama import generate_response
 
 def main():
     st.title("Solution Evaluator")
@@ -8,12 +9,22 @@ def main():
     problem_input = st.text_input("Enter the Problem you want to solve:", "")
     solution_input = st.text_input("Enter the Solution to evaluate:", "")
 
+    # Initial UI components
+    clean_labels, text_response = "", ""
+    st.text_area("Evaluation metrics:", value=clean_labels, key="bert_output", height=110)
+    st.text_area("Example solution :", value=text_response, key="gpt_output", height=110)
+
     if problem_input != "" and solution_input != "" :
     # Run model and display output
         model_output = eval(problem_input, solution_input, model_ckpt="bert_classifier.pth")
         labels = model_output[0]
-        clean_labels = f"Adherence to Circular Economy: {labels[0]} \n Market Potential: {labels[1]} \n Feasibility: {labels[2]}"
-        st.text_area("Idea Evaluation Report:", value=clean_labels, key="model_output", height=100)
+        average_score = sum(labels) / 3
+        average_score = round(average_score, 2)
+        clean_labels = f"Adherence to Circular Economy: {labels[0]} \n Market Potential: {labels[1]} \n Feasibility: {labels[2]} \n Average Score: {average_score}"
+        st.text_area("Evaluation metrics:", value=clean_labels, key="bert_output", height=110)
+
+        text_response = generate_response(problem_input)
+        st.text_area("Example solution :", value=text_response, key="gpt_output", height=110)
 
 if __name__ == "__main__":
     main()
---
+++ gpt_chatbot/tinyllama.py
@@ -0,0 +1,33 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+model = AutoModelForCausalLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def generate_response(problem_input, max_length=300, num_return_sequences=1):
+    # Tokenize and encode the prompt
+    prompt = f"Propose a solution for this problem: {problem_input}"
+    input_ids = tokenizer.encode(prompt, return_tensors="pt")
+
+    # Move input_ids to the same device as the model
+    input_ids = input_ids.to(device)
+
+    # Move model to the same device as input_ids
+    model.to(input_ids.device)
+
+    # Generate response
+    output_ids = model.generate(input_ids, max_length=max_length, num_return_sequences=num_return_sequences, no_repeat_ngram_size=2)
+
+    # Decode and return the generated response
+    generated_response = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    
+    return generated_response
+
+
+# Usage
+# problem = "Businesses worldwide expend substantial financial resources on paper-based transaction evidence like printed receipts. This not only adds to operational costs but also contributes to environmental degradation due to paper wastage and lack of recycling."
+# prompt = "Propose a solution for this problem: " + problem
+# response = generate_response(prompt)
+# print("Generated Response:", response)
